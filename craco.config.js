c307d222d50f33a9d9e9c74a595d14dad612d562
Abd UlHameed Maree <abd.maree@scandinaviatech.com>
2021-06-12 08:54:34 +0300

    [ADD]:setup craco with paath elias.

---
+++ craco.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  style: {
+    postcss: {
+      plugins: [require('tailwindcss'), require('autoprefixer')],
+    },
+  },
+  webpack: {
+    alias: {
+      '@/components': path.resolve(__dirname, 'src/components/'),
+      '@/images': path.resolve(__dirname, 'src/assets/images/'),
+    },
+  },
+};
